41e37494311c6ace745cf4c82b8189af39793f30
iMac <iMac@iMacs-iMac.local>
2020-05-28 12:31:33 -0700

    Vistas principales creadas y conectadas

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5efc61feafab814e7a32";
+/******/ 	var hotCurrentHash = "5036a82f637710a5909e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ .next/static/webpack/static/development/pages/channel.js.01bd882110770ba3816b.hot-update.js
@@ -0,0 +1,17 @@
+webpackHotUpdate("static/development/pages/channel.js",[
+/* 0 */,
+/* 1 */,
+/* 2 */
+false,
+/* 3 */
+/*!*********************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2Fchannel&absolutePagePath=%2FUsers%2FiMac%2Fprojects%2Fpodcast%2Fpages%2Fchannel.js ***!
+  \*********************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2Fchannel&absolutePagePath=%2FUsers%2FiMac%2Fprojects%2Fpodcast%2Fpages%2Fchannel.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fchannel&absolutePagePath=%2FUsers%2FiMac%2Fprojects%2Fpodcast%2Fpages%2Fchannel.js!./");
+
+
+/***/ })
+])
