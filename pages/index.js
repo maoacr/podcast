a20ab37ebb68b9afad3a984ae65d47e9a66f130c
iMac <iMac@iMacs-iMac.local>
2020-05-27 21:20:59 -0700

    Proyecto de Next Inicializado y probado

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default class extends React.Component {
+	render() {
+		return <div>
+
+		<h1>Hola platzi !</h1>
+		<p>Bienvenidos al curso de Next Js</p>
+
+		</div>
+	}
+}
